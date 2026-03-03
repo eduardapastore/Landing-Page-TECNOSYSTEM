d1a03c98c1dfe8346945003f3718eb4ea7d38768
law <kazemarus@outlook.com>
2026-03-03 15:16:43 -0300

    fix: update Header component to remove unused PrismicRichText and correct link access

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import { createClient } from "@/prismicio";
 import { notFound } from "next/navigation";
-import { PrismicRichText } from "@prismicio/react";
 import { PrismicNextLink } from "@prismicio/next";
 import { PrismicNextImage } from "@prismicio/next";
 
@@ -13,7 +12,7 @@ const Header = async () => {
         {/* importando título da página para teste */}
         <div className="font-black text-black text-2xl flex gap-6 items-center">
             <PrismicNextImage field={page.data.tslogo} alt="" width={80}/>
-            <PrismicRichText field={page.data.name} />
+            <span>Tecno System</span>
         </div>
         <nav>
               <ul className="flex gap-6 font-bold text-lg ">
@@ -21,7 +20,7 @@ const Header = async () => {
                   {/* chama o item de navegação e escolhe o index(link) e a info(label) */}
                   {page.data.navigation.map((link, label) => (
                       <li className="transition-all duration-300 text-xl uppercase ease-initial hover:text-red-600" key={label}>
-                          <PrismicNextLink field={link.link[0]}>{link.label}</PrismicNextLink>
+                          <PrismicNextLink field={link.link}>{link.label}</PrismicNextLink>
                       </li>
                   ))
                   }
